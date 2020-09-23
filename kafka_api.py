3213c5a12b66cb470a2c8509b9f6a8348531f87e
taixingbi <bitaihang@gmail.com>
2020-09-23 09:40:26 -0400

    Update kafka_api.py

--- kafka_api.py
+++ kafka_api.py
@@ -10,9 +10,11 @@ class KafkaClass:
     def __init__(self):
         print("\n--------------------------Kafka--------------------------")
         #self.ip= '54.91.38.33'
-        self.ip= 'ec2-54-91-38-33.compute-1.amazonaws.com'
+        #self.ip= 'ec2-54-91-38-33.compute-1.amazonaws.com'
         
-        #self.ip= 'localhost'
+        self.ip= 'b-2.demo-cluster-1.aiagr3.c10.kafka.us-east-1.amazonaws.com:9094,b-1.demo-cluster-1.aiagr3.c10.kafka.us-east-1.amazonaws.com:9094'
+        self.ip= 'b-2.demo-cluster-1.aiagr3.c10.kafka.us-east-1.amazonaws.com:9092,b-1.demo-cluster-1.aiagr3.c10.kafka.us-east-1.amazonaws.com:9092'
+        
         self.bootstrap_servers= [self.ip+':9092']
         #self.bootstrap_servers= [ '54.91.38.33:9092', '54.91.38.33:9093', '54.91.38.33:9094']
 
